7314156b20f91ed1743690d5e5f53e3c6478a872
Your Name <tpunchihewage@gmail.com>
2022-06-29 14:10:48 +0200

    file modified

--- solution.js
+++ solution.js
@@ -1,1 +1,32 @@
+// array values
 
+const str = ["Dortmund", "Paris", "Tomato", 40, "Benz", "Audi"];
+
+console.log("---------------");
+console.log("Third Item is " + str[2]);
+console.log("fourth Item is " + str[3]);
+
+// New Values
+
+const firstName = "Neymar";
+const age = 30;
+const year = 2022;
+
+console.log(firstName + " is " + age + " in " + year);
+
+// New value
+
+const isMarrid = true;
+
+console.log("Neymar is Marrid: " + isMarrid);
+
+// new Value
+
+const programmingLanguage = "JavaScript";
+const isFun = true;
+
+console.log(programmingLanguage + ": " + isFun);
+
+console.log(programmingLanguage[0] + " " + programmingLanguage[4]);
+
+console.log(programmingLanguage.length);
